15b37bb87fd8f8dbff228fd9d1baa9577d3370ee
Carolina Soto <karitovalenzuela88@gmail.com>
2026-02-11 06:39:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
-/* ======================================================
-   CARO FLOWER ART – CONFIG
-   ====================================================== */
+/* ==========================================
+   CARO FLOWER ART - CONFIG
+========================================== */
 
 window.CARO_CONFIG = {
   // Si true: oculta precios y muestra solo "Request quote"
   portfolioMode: false,
 
-  // Si true: si el usuario NO ha elegido moneda antes,
-  // intentamos decidir entre CAD/COP con heurística (idioma / timezone)
+  // Si true: intenta decidir entre CAD/COP con heurística
   autoCurrency: true,
 
-  // Fallback si no se puede adivinar
+  // fallback si no se puede adivinar
   defaultCurrency: "CAD",
-
- 
 };
+
+/* ==========================================
+   SUPABASE CONFIG
+========================================== */
+
+window.SUPABASE_URL = "https://dqmwdssqbnhcyiztgblb.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxbXdkc3NxYm5oY3lpenRnYmxiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDI4MjA4NCwiZXhwIjoyMDg1ODU4MDg0fQ.qirisV3lj44zEfGcfx3GG5huXTmE9ryZdYRyZUz1L4Y";
